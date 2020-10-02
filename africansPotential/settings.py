739225b54e45a26e3d4baccf79ba238d5dcebfbc
ATS Tech <atarqiudeen@gmail.com>
2020-10-02 01:14:32 +0000

    ready to deploy

--- africansPotential/settings.py
+++ africansPotential/settings.py
@@ -1,6 +1,6 @@
 
 from pathlib import Path
-
+import dj_database_url 
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +132,7 @@ AUTH_USER_MODEL = 'users.User'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
